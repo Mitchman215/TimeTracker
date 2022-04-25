468436a7ee33a2d7cf9e1acf85f2b1e95e786851
melvinhe <melvinhe2014@gmail.com>
2022-04-25 17:00:52 -0400

    auth

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,12 +5,20 @@ import Home from './routes/Home'
 import Timer from './routes/Timer'
 import SignIn from './routes/SignIn'
 
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { auth } from './firebase'
+
+// some logic here to make sure Nav doesn't render unless logged in.
 export default function App() {
+  const [user, loading, error] = useAuthState(auth)
+
   return (
     <div className="bg-brown w-screen h-screen p-4">
-      <header>
-        <Nav />
-      </header>
+      {user && (
+        <header>
+          <Nav />
+        </header>
+      )}
       <main className="mt-8">
         <Routes>
           <Route path="/" element={<Home />} />
