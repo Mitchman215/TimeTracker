863ed52936111543d119a013a5b40e4d43fdb515
AskSid <siddboppana@gmail.com>
2022-04-26 16:36:46 -0400

    Basic search functionality

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,13 @@ import { Route, Routes } from 'react-router-dom'
 import Nav from './components/Nav'
 import Home from './routes/Home'
 import Timer from './routes/Timer'
+import Search from './components/Search'
 
 export default function App() {
   return (
     <div className="bg-brown w-screen h-screen p-4">
       <header>
-        <Nav />
+        <Search />
       </header>
       <main className="mt-8">
         <Routes>
