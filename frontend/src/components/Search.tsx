3a32c94385f870d332b021bf7d5d49b6a7cb9edd
AskSid <siddboppana@gmail.com>
2022-05-02 17:17:25 -0400

    Search recommendations work

--- frontend/src/components/Search.tsx
+++ frontend/src/components/Search.tsx
@@ -10,9 +10,10 @@ import {
 } from 'firebase/firestore'
 import { useCollection } from 'react-firebase-hooks/firestore'
 import { FormEvent, useState } from 'react'
+import leven from 'leven'
 
 export default function Search() {
-  const [result, setResult] = useState('')
+  const [result, setResult] = useState('') // the user search, need to rename
   const [category, setCategory] = useState('name')
   const [suggestions, setSuggestions]: any[] = useState([])
   const [classes, setClasses]: any[] = useState([])
@@ -24,74 +25,60 @@ export default function Search() {
     query(classesRef, orderBy('department'))
   )
 
-  function loadClassesDepts() {
-    // adds classes and departments from database to an array and set
-    const docs: QueryDocumentSnapshot<DocumentData>[] | undefined =
-      classValue?.docs
-    if (docs !== undefined) {
-      for (let i = 0; i < docs?.length; i++) {
-        const jsonString: string = JSON.stringify(docs[i].data())
-        const obj = JSON.parse(jsonString)
-        setClasses(classes.push(obj.name))
-        setDepartments(departments.add(obj.department))
-        //classes.push(obj.name)
-        //departments.add(obj.department)
-      }
-    }
-  }
-
-  const docs: QueryDocumentSnapshot<DocumentData>[] | undefined =
-    classValue?.docs
-  if (docs !== undefined) {
-    for (let i = 0; i < docs?.length; i++) {
-      const jsonString: string = JSON.stringify(docs[i].data())
-      const obj = JSON.parse(jsonString)
-      //setClasses(classes.push(obj.name))
-      //setDepartments(departments.add(obj.department))
-      classes.push(obj.name)
-      departments.add(obj.department)
-    }
-  }
-
   const [value, loading, error] = useCollection(
     query(classesRef, where(category, '==', result))
   )
 
   function closestVals(userSearch: string, userCategory: string) {
+    const docs: QueryDocumentSnapshot<DocumentData>[] | undefined =
+      classValue?.docs
+    const a = []
+    const b = new Set()
+    if (docs !== undefined) {
+      setClasses([])
+      setDepartments(new Set())
+      for (let i = 0; i < docs?.length; i++) {
+        const jsonString: string = JSON.stringify(docs[i].data())
+        const obj = JSON.parse(jsonString)
+        a.push(obj.name)
+        b.add(obj.department)
+      }
+      setClasses(a)
+      setDepartments(b)
+    }
     let type: any[]
     if (userCategory === 'name') {
       type = classes
     } else {
       type = Array.from(departments)
     }
-    if (type.includes(userSearch)) {
-      setSuggestions([userSearch, userSearch])
-      //return [userSearch, userSearch]
-    } else {
-      setSuggestions([])
-      //return []
+    const distances = []
+    for (let i = 0; i < type.length; i++) {
+      const distance = leven(userSearch, type[i])
+      distances.push([type[i], distance])
     }
+    distances.sort((first, second) => {
+      return first[1] - second[1]
+    })
+    setSuggestions([distances[0][0], distances[1][0], distances[2][0]])
   }
 
   return (
     <section className="bg-white rounded-lg p-4">
       <h1 className="text-black font-bold">Search</h1>
-      {suggestions.map((c: string) => (
-        <p>{c}</p>
-      ))}
       <select id="category" onChange={(e) => setCategory(e.target.value)}>
         <option value="name">Name</option>
         <option value="department">Department</option>
       </select>
       <input
         placeholder="Search for an assignment / class / department"
-        //onChange={(e) => setResult(e.target.value)}
         onChange={(e) => closestVals(e.target.value, category)}
       />
-      {value &&
-        value.docs.map((doc) => (
-          <p key={doc.id}>{JSON.stringify(doc.data())}, </p>
-        ))}
+      {suggestions.map((c: string) => (
+        <div>
+          <button type="button">{c}</button>
+        </div>
+      ))}
     </section>
   )
 }
