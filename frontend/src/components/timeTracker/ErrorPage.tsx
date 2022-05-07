25c49f8fabfe8bf4856859a5dba40729b7a4eef7
Mitchman215 <mitchell_salomon@brown.edu>
2022-05-07 18:43:12 -0400

    fix: error page for if user has no classes

---
+++ frontend/src/components/timeTracker/ErrorPage.tsx
@@ -0,0 +1,38 @@
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+interface ErrorPageProps {
+  error: string | Error
+}
+
+// A component that shows an error and a button to navigate back to the home page
+function ErrorPage({ error }: ErrorPageProps) {
+  // when the page loads, log the error
+  useEffect(() => {
+    console.log({ error })
+  }, [error])
+
+  // redirect to home page when button is clicked
+  const navigate = useNavigate()
+  function redirectToHome() {
+    navigate('/')
+  }
+
+  return (
+    <div className="flex flex-col items-center">
+      <span className="rounded-lg p-4 bg-orange-light flex flex-col items-center">
+        <h1 className="text-2xl m-2 text-white font-semibold drop-shadow-md">
+          An error occured!
+        </h1>
+        <p className="text-red-400 text-xl m-2 drop-shadow-sm italic">
+          {error.toString()}
+        </p>
+        <button className="btn-purple my-2" onClick={redirectToHome}>
+          Navigate back to home page
+        </button>
+      </span>
+    </div>
+  )
+}
+
+export default ErrorPage
--- frontend/src/models/Class.ts
+++ frontend/src/models/Class.ts
@@ -1,2 +1,62 @@
-// Represents a class, mirrors firestore
-export default class Class {}
+import {
+  DocumentData,
+  DocumentReference,
+  QueryDocumentSnapshot,
+  SnapshotOptions,
+} from 'firebase/firestore'
+
+interface DayAverage {
+  average: number
+  day: Date
+}
+
+interface WeekAverage {
+  average: number
+  start: Date
+  finish: Date
+}
+
+interface MonthAverage {
+  average: number
+  month: string
+}
+
+// Represents a class from a users' "classes" collection, mirrors firestore db
+export default class Class {
+  constructor(
+    readonly id: string,
+    readonly name: string,
+    readonly ref: DocumentReference,
+    readonly dailyAverages: DayAverage[],
+    readonly weeklyAverages: WeekAverage[],
+    readonly monthlyAverages: MonthAverage[]
+  ) {}
+}
+
+// Firestore data converter for Class
+export const classConverter = {
+  toFirestore(theClass: Class): DocumentData {
+    return {
+      id: theClass.id,
+      name: theClass.name,
+      class: theClass.ref,
+      daily_averages: theClass.dailyAverages,
+      weekly_averages: theClass.weeklyAverages,
+      monthly_averages: theClass.monthlyAverages,
+    }
+  },
+  fromFirestore(
+    snapshot: QueryDocumentSnapshot,
+    options: SnapshotOptions
+  ): Class {
+    const data = snapshot.data(options)
+    return new Class(
+      snapshot.id,
+      data.name,
+      data.class,
+      data.daily_averages,
+      data.weekly_averages,
+      data.monthly_averages
+    )
+  },
+}
