4f74ae02c37ad3d25865a2628eb7c094b532586a
Mitchman215 <mitchell_salomon@brown.edu>
2022-05-10 23:42:22 -0400

    feat: departments adding script done

--- backend/src/main/java/edu/brown/cs/student/commands/AddDepartments.java
+++ backend/src/main/java/edu/brown/cs/student/commands/AddDepartments.java
@@ -2,18 +2,23 @@ package edu.brown.cs.student.commands;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.SetOptions;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
 import edu.brown.cs.student.repl.Command;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.Select;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.time.Duration;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -32,16 +37,17 @@ public class AddDepartments implements Command {
   
   private static final String CAB_URL = "https://cab.brown.edu/";
   
-  private static final String PROJECT_ID = "time-tracker-669e3";
+  private static final String PATH_TO_KEY = "secret/time-tracker-669e3-8ec92c95370d.json";
   
   private final Firestore db;
   
   public AddDepartments() throws IOException {
-    GoogleCredentials credentials = GoogleCredentials.getApplicationDefault();
+    WebDriverManager.chromedriver().setup();
+    InputStream serviceAccount = new FileInputStream(PATH_TO_KEY);
+    GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccount);
     FirebaseOptions options = new FirebaseOptions.Builder()
-        .setCredentials(credentials)
-        .setProjectId(PROJECT_ID)
-        .build();
+          .setCredentials(credentials)
+          .build();
     FirebaseApp.initializeApp(options);
 
     this.db = FirestoreClient.getFirestore();
@@ -56,19 +62,21 @@ public class AddDepartments implements Command {
     
     ChromeOptions options = new ChromeOptions();
     // disables browser from opening
-    // options.setHeadless(true);
-    
+    options.setHeadless(true);
+
     ChromeDriver driver = new ChromeDriver(options);
     // if you can't find something, wait a bit (handles if page hasn't loaded yet)
     driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
     driver.get(CAB_URL);
-    
+
+    // get subjects from CAB through the subject filter dropdown
     Select subjectsDropdown = new Select(driver.findElement(By.id("crit-subject")));
     List<WebElement> allSubjectOptions = subjectsDropdown.getOptions();
     List<String> allSubjects = allSubjectOptions.stream()
         .map((elt) -> elt.getText())
         .collect(Collectors.toList());
-    
+
+    List<String> codesAdded = new LinkedList<>();
     try {
       // get all subjects already in firestore
       Set<String> subjectCodesInDb = db.collection("departments").get()
@@ -79,12 +87,17 @@ public class AddDepartments implements Command {
       // save new subjects to firestore
       allSubjects
           .forEach((subj) -> {
-            String[] split = subj.split("(");
+            String[] split = subj.split("\\(");
+            // handle the first option that is "Any subject"
+            if (split.length == 1) {
+              return;
+            }
             String name = split[0];
             String code = split[1].substring(0, split[1].length() - 1);
             System.out.println(name + ": " + code);
             // make sure subject is not already in firestore
             if (!subjectCodesInDb.contains(code)) {
+              codesAdded.add(code);
               Map<String, String> docData = Map.of(
                     "name", name,
                     "daily_average", "0",
@@ -92,16 +105,16 @@ public class AddDepartments implements Command {
                     "weekly_average", "0"
               );
               // send new doc to firestore
-              // db.collection("department").document(code).set(docData, SetOptions.merge());
+              db.collection("departments").document(code).set(docData, SetOptions.merge());
             }
-
           });
     } catch (ExecutionException | InterruptedException e) {
-      return "ERROR: " + e.getMessage();
+      e.printStackTrace();
+      return "ERROR: " + e;
     }
 
     driver.quit();
-    return "success";
+    return "Added " + codesAdded.size() + " subjects: " + codesAdded;
   }
 
   @Override
