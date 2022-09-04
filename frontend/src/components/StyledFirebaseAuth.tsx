3890510b5e3a6e25ae89702b00ecb05b2e806edb
Mitchell Salomon <mitchell_salomon@brown.edu>
2022-09-04 11:39:18 -0400

    nextjs firebaseui react fix

--- frontend/src/components/StyledFirebaseAuth.tsx
+++ frontend/src/components/StyledFirebaseAuth.tsx
@@ -1,56 +1,72 @@
 // This file is a replacement for react-firebaseui since they refuse to merge the pr to update react dependency
+// Source: https://github.com/firebase/firebaseui-web-react/pull/173#issuecomment-1215648239
 
-import { useEffect, useRef, useState } from 'react';
-import { onAuthStateChanged } from 'firebase/auth';
-import * as firebaseui from 'firebaseui';
-import 'firebaseui/dist/firebaseui.css';
+import { useEffect, useRef, useState } from 'react'
+import { onAuthStateChanged } from 'firebase/auth'
+import 'firebaseui/dist/firebaseui.css'
+import { auth } from 'firebaseui'
 
 interface Props {
-    // The Firebase UI Web UI Config object.
-    // See: https://github.com/firebase/firebaseui-web#configuration
-    uiConfig: firebaseui.auth.Config;
-    // Callback that will be passed the FirebaseUi instance before it is
-    // started. This allows access to certain configuration options such as
-    // disableAutoSignIn().
-    uiCallback?(ui: firebaseui.auth.AuthUI): void;
-    // The Firebase App auth instance to use.
-    firebaseAuth: any; // As firebaseui-web
-    className?: string;
+  // The Firebase UI Web UI Config object.
+  // See: https://github.com/firebase/firebaseui-web#configuration
+  uiConfig: auth.Config
+  // Callback that will be passed the FirebaseUi instance before it is
+  // started. This allows access to certain configuration options such as
+  // disableAutoSignIn().
+  uiCallback?(ui: auth.AuthUI): void
+  // The Firebase App auth instance to use.
+  firebaseAuth: any // As firebaseui-web
+  className?: string
 }
 
+const StyledFirebaseAuth = ({
+  uiConfig,
+  firebaseAuth,
+  className,
+  uiCallback,
+}: Props) => {
+  const [firebaseui, setFirebaseui] = useState<
+    typeof import('firebaseui') | null
+  >(null)
+  const [userSignedIn, setUserSignedIn] = useState(false)
+  const elementRef = useRef(null)
 
-const StyledFirebaseAuth = ({uiConfig, firebaseAuth, className, uiCallback}: Props) => {
-    const [userSignedIn, setUserSignedIn] = useState(false);
-    const elementRef = useRef(null);
+  useEffect(() => {
+    // Firebase UI only works on the Client. So we're loading the package only after
+    // the component has mounted, so that this works when doing server-side rendering.
+    setFirebaseui(require('firebaseui'))
+  }, [])
 
-    useEffect(() => {
-        // Get or Create a firebaseUI instance.
-        const firebaseUiWidget = firebaseui.auth.AuthUI.getInstance() || new firebaseui.auth.AuthUI(firebaseAuth);
-        if (uiConfig.signInFlow === 'popup')
-            firebaseUiWidget.reset();
+  useEffect(() => {
+    if (firebaseui === null) return
 
-        // We track the auth state to reset firebaseUi if the user signs out.
-        const unregisterAuthObserver = onAuthStateChanged(firebaseAuth, (user) => {
-            if (!user && userSignedIn)
-                firebaseUiWidget.reset();
-            setUserSignedIn(!!user);
-        });
+    // Get or Create a firebaseUI instance.
+    const firebaseUiWidget =
+      firebaseui.auth.AuthUI.getInstance() ||
+      new firebaseui.auth.AuthUI(firebaseAuth)
+    if (uiConfig.signInFlow === 'popup') firebaseUiWidget.reset()
 
-        // Trigger the callback if any was set.
-        if (uiCallback)
-            uiCallback(firebaseUiWidget);
+    // We track the auth state to reset firebaseUi if the user signs out.
+    const unregisterAuthObserver = onAuthStateChanged(firebaseAuth, (user) => {
+      if (!user && userSignedIn) firebaseUiWidget.reset()
+      setUserSignedIn(!!user)
+    })
 
-        // Render the firebaseUi Widget.
-        // @ts-ignore
-        firebaseUiWidget.start(elementRef.current, uiConfig);
+    // Trigger the callback if any was set.
+    if (uiCallback) uiCallback(firebaseUiWidget)
 
-        return () => {
-            unregisterAuthObserver();
-            firebaseUiWidget.reset();
-        };
-    }, [firebaseui, uiConfig]);
+    // Render the firebaseUi Widget.
+    // @ts-ignore
+    firebaseUiWidget.start(elementRef.current, uiConfig)
 
-    return <div className={className} ref={elementRef} />;
-};
+    return () => {
+      unregisterAuthObserver()
+      firebaseUiWidget.reset()
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [firebaseui, uiConfig])
 
-export default StyledFirebaseAuth;
+  return <div className={className} ref={elementRef} />
+}
+
+export default StyledFirebaseAuth
